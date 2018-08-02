bff474a44fd59fb97e9ecb91a483782349e246ca
Pablo <pablo.riutort@gmail.com>
2018-08-02 14:47:30 +0200

    Added python logging support

--- elasticsearch_bulk.py
+++ elasticsearch_bulk.py
@@ -4,10 +4,11 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from datetime import datetime
 from requests import put
 
+import logging as log
+log.basicConfig(filename='elasticsearch_bulk.log', level=log.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 HOST = 'localhost'
 PORT = 5000
-
 # Put your elasticsearch config here
 ELASTIC_URL = ''
 ELASTIC_PORT = None
@@ -15,6 +16,7 @@ ELASTIC_PORT = None
 
 class BulkHandler(BaseHTTPRequestHandler):
     def do_POST(self):
+        log.info("Message received")
         # get message from POST
         self.data_string = self.rfile.read(int(self.headers['Content-Length'])).decode('UTF-8')
         self.send_response(200)
@@ -25,13 +27,14 @@ class BulkHandler(BaseHTTPRequestHandler):
         for line in self.data_string.splitlines():
             if line != '':
                 data = json.loads(line)
+                log.debug(data)
 
                 if data.get('timestamp'):
                     timestamp = datetime.strptime(data['timestamp'], "%d/%m/%YT%H:%M:%S")
                     data['timestamp'] = timestamp.strftime('%Y-%m-%dT%H:%M:%S')
 
                 if data.get('message'):
-                    data['message'] = float(data['message'])
+                    data['message'] = int(data['message'])
 
                 if data.get('index'):
                     if data['index'].get('_type'):
@@ -43,11 +46,14 @@ class BulkHandler(BaseHTTPRequestHandler):
 
         headers = {'Content-type': 'application/x-ndjson'}
         try:
-            response = put(ELASTIC_URL +':'+ str(ELASTIC_PORT) + '/_bulk',
-                    data=elastic_data.encode('utf-8'), headers=headers)
-            print(response.text)
+            bulk_url = ELASTIC_URL +':'+ str(ELASTIC_PORT) + '/_bulk'
+            encoded_data = elastic_data.encode('utf-8')
+            log.info("Sending data to " + bulk_url)
+            log.debug(encoded_data)
+            response = put(bulk_url, data=encoded_data, headers=headers)
+            log.debug(response.text)
         except ConnectionError as con_err:
-            print("Something went wrong", con_err)
+            log.error(con_err)
 
         return
 
@@ -55,7 +61,7 @@ class BulkHandler(BaseHTTPRequestHandler):
 def run(host=HOST, port=PORT, server_class=HTTPServer, handler_class=BulkHandler):
     httpd = server_class((host, port), handler_class)
     try:
-        print("Starting httpd at", host, port)
+        log.info("Starting httpd at {} {}".format(host, port))
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
@@ -66,3 +72,5 @@ if __name__ == '__main__':
         run(host=argv[1], port=int(argv[2]))
     else:
         run()
+
+
